b37aefd9e1db56185cd7b4cb741862293d504c2c
leonardo fernandez <squishyyy95@yahoo.com>
2017-12-20 21:36:18 -0800

    initial commit

---
+++ game.js
@@ -0,0 +1,21 @@
+
+console.log("test");
+alert("hello");
+
+var randomResult;
+var win;
+var lost;
+
+
+
+
+//speudo coding
+
+// a game with 4 crsytal and random result
+// Every crystal has a random number between 1-12
+// a new random number should appear everytime we win or lose
+// for the 4 crsytals
+// when you click on a crystal, it should be adding with the previous answer
+// until it equals the random result
+// If it is greater than the random result, we substract a lost counter
+// If it is equal then we add a win counter
